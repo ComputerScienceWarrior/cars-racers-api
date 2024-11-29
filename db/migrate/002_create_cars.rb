7fdaa54d1caf6fee4129e101521b59693c0fe701
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-11-29 08:38:21 -0700

    adds fields for Car migration

---
+++ db/migrate/002_create_cars.rb
@@ -0,0 +1,19 @@
+class CreateCars < ActiveRecord::Migration[7.1]
+  def change
+    create_table :cars do |t|
+      t.string :name
+      t.decimal :atk
+      t.decimal :def
+      t.decimal :spd
+      t.decimal :hp
+      t.integer :wins
+      t.integer :losses
+      t.integer :tournament_wins
+      t.integer :tournament_lossses
+      t.integer :championship_wins
+
+      t.references :user, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
